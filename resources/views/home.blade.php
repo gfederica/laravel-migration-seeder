d773ff72b2f031c525eaae994b7ce23191b050d6
gfederica <effegiudice@gmail.com>
2021-07-15 23:39:22 +0200

    Aggiunto scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,40 +4,34 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Travel Agency</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-           
-        </style>
     </head>
     <body>
-        <section class="container">
-            <h2>Pacchetti viaggio:</h2>
-            <div class="cards">
-                @foreach ($travels as $travel)
-                    <div class="card">
-                       <h2>{{$travel->destination}}</h2>
-                       <h4>Giorni: {{$travel->days}}</h4>
-                       <h4>Tipo di viaggio: {{$travel->type}}</h4>
-                       <h4>Pensione: {{$travel->roomAndBoard}}</h4>
-                       <h4>Tour Guidati: {{ $travel->guidedTours ? 'Sì' : 'No' }}</h4>
-                       <small>Prezzo pacchetto: {{ $travel->price }} €</small>
-                    </div>
-                @endforeach
-            </div>
-        </section>
+        @include('partials.header')
+
+        <main>
+            <section class="container">
+                <h2>Pacchetti viaggio</h2>
+                <div class="cards">
+                    @foreach ($travels as $travel)
+                        <div class="card">
+                        <h3>{{$travel->destination}}</h3>
+                        <p>Giorni: {{$travel->days}}</p>
+                        <p>Tipo di viaggio: {{$travel->type}}</p>
+                        <p>Pensione: {{$travel->roomAndBoard}}</p>
+                        <p>Tour Guidati: {{ $travel->guidedTours ? 'Sì' : 'No' }}</p>
+                        <small>Prezzo pacchetto: {{ $travel->price }} €</small>
+                        </div>
+                    @endforeach
+                </div>
+            </section>
+        </main>
     </body>
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="container head">
+        <div class="logo">
+            <img src="{{asset('images/logo.png')}}" alt="" class="logo">
+        </div>
+        <div class="nav">
+            <ul>
+                <li class="active"><a href="">Pacchetti viaggio</a></li>
+                <li><a href="">Last Minute</a></li>
+                <li><a href="">Contatti</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
